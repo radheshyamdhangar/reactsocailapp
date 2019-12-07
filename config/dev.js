f66df2cf71ecda1e6288ee7cd41e2e8a557a0ac6
radheshyam <radheshyam.dhan@gmail.com>
2019-12-07 10:23:35 +0530

    make it better

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports={
+    googleClientID:'402314611580-a2gt4frlkkvels8amq5v2ksqrabgestk.apps.googleusercontent.com',
+    googleClientSecret:'Jt0-gdp9KqwiEFV_-RkVlCJo',
+    mongoURI:'mongodb+srv://radhe:radhe123@cluster0-eyvrz.mongodb.net/sociallogin?retryWrites=true&w=majority',
+    cookieKey:'radheradhe',
+    FACEBOOK_APP_ID:'2224327384355853',
+    FACEBOOK_APP_SECRET:'2b369bca6e887fc31c813a392a7715d8'
+}
